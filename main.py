d1279f91bc07517e13f8373936fd24774e8ce42f
Noelis28 <lyamber64@gmail.com>
2026-01-05 20:21:59 -0500

    Enable dynamic port configuration for Flask app
    Added environment variable support for port configuration.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 # Importar
 from flask import Flask, render_template, request
+import os
 
-
+# Crear la aplicación Flask: inicia la web
 app = Flask(__name__)
+
+
+
+
+if __name__ == "__main__":
+    port = os.environ.get("PORT", 8080)
+    app.run(host="0.0.0.0", port=port)
 
 def result_calculate(size, lights, device):
     # Variables que permiten calcular el consumo energético de los aparatos
